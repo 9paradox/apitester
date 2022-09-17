d86430c14fedfd7b93f1e7d5aaeb0e13ce80c086
Paradox <paradox.akshay@gmail.com>
2022-09-17 23:33:12 +0530

    fixed file path to be absolute

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,10 +2,16 @@ import path from 'path';
 import fs from 'fs';
 
 function fileExists(filePath: string): boolean {
+  if (path.isAbsolute(filePath)) {
+    return fs.existsSync(path.resolve(filePath));
+  }
   return fs.existsSync(path.resolve(process.cwd(), filePath));
 }
 
 function readFile(filePath: string): string {
+  if (path.isAbsolute(filePath)) {
+    return fs.readFileSync(path.resolve(filePath), 'utf-8');
+  }
   return fs.readFileSync(path.resolve(process.cwd(), filePath), 'utf-8');
 }
 
