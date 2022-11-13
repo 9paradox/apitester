86c4520bc3ef83a3b1e6322d950446bb5aaa404e
Paradox <paradox.akshay@gmail.com>
2022-11-13 20:41:02 +0530

    updated npm version and example code

--- examples/index.js
+++ examples/index.js
@@ -1,20 +1,22 @@
 const { default: apitester } = require("@9paradox/apitester");
 
-apitester.setup('should perform overall test actions and verifications')
-    .simpleGet('https://jsonplaceholder.typicode.com/todos/')
-    .withLastStep_pickAndVerify('status', 200)
+apitester.createTestCase('should perform overall test actions and verifications')
+    .get('https://jsonplaceholder.typicode.com/todos/')
+    .pickAndVerify({query: 'status', expected: 200})
     .pickStep(1)
-    .withLastStep_pickData('data[0].{id: id}')
-    .withLastStep_formatData('https://jsonplaceholder.typicode.com/todos/<%= it.id %>')
-    .withLastStep_simpleGet()
-    .withLastStep_pickData('data.title')
-    .withLastStep_Verify('delectus aut autem')
+    .pickData('data[0].{id: id}')
+    .formatData('https://jsonplaceholder.typicode.com/todos/<%= it.id %>')
+    .get()
+    .pickData('data.title')
+    .verify('delectus aut autem')
     .test()
     .then(testResult => {
         console.log('Testcase success:-')
         console.log(testResult.success);
-        console.log('Testcase steps:-')
-        console.log(testResult.steps);
+        if(!testResult.success){
+            console.log('Testcase steps:-')
+            console.log(testResult.steps);
+        }
     })
     .catch(err => {
         console.log(err);
