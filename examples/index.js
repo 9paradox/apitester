20f0d80e9ee6fe8af7f428b7a8de6ab0ccd6fa2f
Paradox <paradox.akshay@gmail.com>
2023-03-12 20:00:10 +0530

    updated documents and example

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,7 @@ const { apitester } = require("@9paradox/apitester");
 
 apitester.createTestCase('should perform overall test actions and verifications')
     .get('https://jsonplaceholder.typicode.com/todos/')
-    .pickAndVerify({query: 'status', expected: 200})
+    .pickAndVerify({ query: 'status', expected: 200 })
     .pickStep(1)
     .pickData('data[0].{id: id}')
     .formatData('https://jsonplaceholder.typicode.com/todos/<%= it.id %>')
@@ -11,13 +11,10 @@ apitester.createTestCase('should perform overall test actions and verifications'
     .verify('delectus aut autem')
     .test()
     .then(testResult => {
-        console.log('Testcase success:-')
-        console.log(testResult.success);
-        if(!testResult.success){
-            console.log('Testcase steps:-')
-            console.log(testResult.steps);
+        console.log('Testcase success:- ', testResult.success);
+        if (!testResult.success) {
+            console.log('Testcase failed:- ', testResult.error);
+            console.log('Testcase steps:- ', testResult.steps);
         }
     })
-    .catch(err => {
-        console.log(err);
-    });
+    .catch(err => console.log(err));
