77de718a8e3169c7acbf17db0dd613f8147145cb
Paradox <paradox.akshay@gmail.com>
2023-04-08 00:42:38 +0530

    added new example testcases

--- examples/index.js
+++ examples/index.js
@@ -1,14 +1,34 @@
+//npm i @9paradox/apitester
 const { apitester } = require("@9paradox/apitester");
 
-apitester.createTestCase('should perform overall test actions and verifications')
+//testcase to verify TODOs endpoint is status is success
+//then pick the first todo from response and verify if the tile.
+
+apitester.createTestCase('Verify TODOS endpoint')
+
+    //step 1. get results form /todos
     .get('https://jsonplaceholder.typicode.com/todos/')
+
+    //step 2. verify the status is success
     .pickAndVerify({ query: 'status', expected: 200 })
+
+    //step 3. select data of step 1
     .pickStep(1)
+
+    //step 4. select id of first todo
     .pickData('data[0].{id: id}')
+    .pickAndVerify({ query: 'id', toBe: "notIn", expected: ["", null] })
+    .pickStep(4)
+    //step 5. build url for selected id
     .formatData('https://jsonplaceholder.typicode.com/todos/<%= it.id %>')
+
+    //step 6. perform get on the url build in previous step
     .get()
-    .pickData('data.title')
-    .verify('delectus aut autem')
+
+    //step 7. verify title from data received in previous step
+    .pickAndVerify({ query: 'data.title', expected: 'delectus aut autem' })
+
+    //perform the test
     .test()
     .then(testResult => {
         console.log('Testcase success:- ', testResult.success);
