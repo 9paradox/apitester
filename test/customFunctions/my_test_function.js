f3e0cca7ede765a09f7220f741344923d7e8c102
Paradox <paradox.akshay@gmail.com>
2023-09-15 00:45:49 +0530

    fixed lint issue

--- test/customFunctions/my_test_function.js
+++ test/customFunctions/my_test_function.js
@@ -1,11 +1,11 @@
 async function customFunction(testCase, currentStep, lastStep) {
-    var output = testCase.data('delectus_aut_autem');
-    output = output.toUpperCase();
-    var result = {
-        inputData: lastStep.outputData,
-        outputData: output,
-    };
-    return result;
+  var output = testCase.data('delectus_aut_autem');
+  output = output.toUpperCase();
+  var result = {
+    inputData: lastStep.outputData,
+    outputData: output,
+  };
+  return result;
 }
 
 module.exports.customFunction = customFunction;
